7c5473e066438a65dc1c53cf29e8d13a5861c0ac
bagaking <zfaustk@qq.com>
2023-01-16 00:20:52 +0800

    chore(package): keep maintainer files out of tarball

--- scripts/check-pack.mjs
+++ scripts/check-pack.mjs
@@ -9,6 +9,15 @@ const requiredPaths = [
 	"LICENSE",
 ];
 
+const forbiddenPaths = [
+	".eslintignore",
+	".eslintrc",
+];
+
+const forbiddenPrefixes = [
+	".github/",
+];
+
 const output = execFileSync("npm", ["pack", "--dry-run", "--json"], {
 	encoding: "utf8",
 	stdio: ["ignore", "pipe", "inherit"],
@@ -20,7 +29,11 @@ assert.equal(packs.length, 1, "npm pack dry-run should describe one package");
 
 const entries = new Set(packs[0].files.map((file) => file.path));
 const missing = requiredPaths.filter((path) => !entries.has(path));
+const leakedPaths = packs[0].files
+	.map((file) => file.path)
+	.filter((path) => forbiddenPaths.includes(path) || forbiddenPrefixes.some((prefix) => path.startsWith(prefix)));
 
 assert.deepEqual(missing, [], `npm package is missing required files: ${missing.join(", ")}`);
+assert.deepEqual(leakedPaths, [], `npm package includes maintainer-only files: ${leakedPaths.join(", ")}`);
 
 console.log(`Package dry-run includes ${packs[0].entryCount} files and required Obsidian assets.`);
