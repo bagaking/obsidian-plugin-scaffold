b06e419e5b934db22eb876d765429a781fdffd36
bagaking <zfaustk@qq.com>
2023-01-16 00:10:38 +0800

    test(manifest): cover scoped package ids

--- scripts/manifest-package.test.mjs
+++ scripts/manifest-package.test.mjs
@@ -6,15 +6,34 @@ function readJson(path) {
 	return JSON.parse(fs.readFileSync(path, "utf8"));
 }
 
+function deriveManifestId(packageName) {
+	return packageName.startsWith("@") ? packageName.split("/").at(-1) : packageName;
+}
+
 test("manifest metadata stays aligned with package metadata", () => {
 	const packageJson = readJson("package.json");
 	const manifest = readJson("manifest.json");
 	const versions = readJson("versions.json");
 
-	assert.equal(manifest.id, packageJson.name, "manifest id should match package name");
+	assert.equal(manifest.id, deriveManifestId(packageJson.name), "manifest id should match package name or scoped package basename");
 	assert.equal(manifest.version, packageJson.version, "manifest version should match package version");
 	assert.equal(versions[manifest.version], manifest.minAppVersion, "versions.json should map the current plugin version to minAppVersion");
 	assert.equal(packageJson.main, "build/main.js", "package main should point at the built Obsidian plugin entry");
+});
+
+test("package checks cover the configured plugin entry", () => {
+	const packageJson = readJson("package.json");
+	const packCheck = fs.readFileSync("scripts/check-pack.mjs", "utf8");
+
+	assert.equal(
+		packCheck.includes(`"${packageJson.main}"`) || packCheck.includes(`'${packageJson.main}'`),
+		true,
+		"pack:check should require the configured package main in the npm tarball",
+	);
+
+	if (fs.existsSync("build")) {
+		assert.equal(fs.existsSync(packageJson.main), true, "existing build output should include the configured package main");
+	}
 });
 
 test("manifest keeps Obsidian-required fields populated", () => {
